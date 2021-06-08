681632f922fa6301e519ae9dd0a5d8c317f8e675
Pratik Kumar <kumarpratik2211@gmail.com>
2021-06-08 18:44:26 +0530

    make it better

---
+++ src/components/ui/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='center'>
+      <br/>
+        <span>Created By <a href={"https://pratikkumar2000.github.io/Portfolio/"}>Pratik Kumar</a></span>
+    </footer>
+  )
+}
+
+export default Footer
